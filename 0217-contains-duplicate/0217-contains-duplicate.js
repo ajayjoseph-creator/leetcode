4e6f179b5ae4847f529b3e6666f6bdae9d5d6581
ajay_joseph <aj2506039@gmail.com>
2025-03-13 16:58:28 +0530

    Time: 72 ms (10.15%), Space: 64.8 MB (43.49%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,8 @@
+function containsDuplicate(nums) {
+  nums.sort((a, b) => a - b); 
+  
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) return true;
+  }
+  return false;
+}
